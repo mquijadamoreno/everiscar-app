649b455208704edbe2b1e74e63ba2494add84fa1
quijadamorenomiguel <mikumo14@gmail.com>
2019-04-10 09:33:03 +0200

    Updated the service to fetch the cars from the database with an offset and limited query.

--- app/src/main/java/com/everis/bmw/app/boundary/CarService.java
+++ app/src/main/java/com/everis/bmw/app/boundary/CarService.java
@@ -1,6 +1,5 @@
 package com.everis.bmw.app.boundary;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -18,41 +17,42 @@ import com.everis.bmw.app.exceptions.CarStateNotValidException;
 
 @Stateless
 public class CarService {
-	
+
 	@PersistenceContext
 	private EntityManager em;
 
 	private final Logger LOG = Logger.getLogger(this.getClass().getName());
-	
+
 	public Car getCar(String id) {
-		if(id == null) 
+		if (id == null)
 			return null;
-		
+
 		Car car = em.find(Car.class, id);
-		if(car == null) {
+		if (car == null) {
 			throw new CarNotFoundException("Car not found");
 		}
 		return car;
 	}
-	
-	public long getCarsCount(){
+
+	public long getCarsCount() {
 		Query getCarCount = em.createNamedQuery(Car.CAR_GET_CAR_COUNT, Car.class);
 		long count = (long) getCarCount.getSingleResult();
 		return count;
 	}
-	
-	public List<Car> getCars(){
+
+	public List<Car> getCars() {
 		TypedQuery<Car> getAllCars = em.createNamedQuery(Car.CAR_GET_ALL_NAMED_QUERY, Car.class);
 		List<Car> cars = getAllCars.getResultList();
 		return cars;
 	}
-	
-	public List<Car> getCarsPaginated(int start, int size){
-		List<Car> cars = getCars();
-		if(start + size > cars.size()) return new ArrayList<Car>();
-		return cars.subList(start, start+size);
+
+	public List<Car> getCarsPaginated(int start, int size) {
+		TypedQuery<Car> getCarsToPaginate = em.createNamedQuery(Car.CAR_GET_ALL_NAMED_QUERY, Car.class);
+		int offset = (start - 1) * size;
+		List<Car> carsPaginated = getCarsToPaginate.setFirstResult(offset).setMaxResults(size).getResultList();
+		return carsPaginated;
 	}
-	
+
 	public Car createCar(Car car) {
 		try {
 			car.setId(UUID.randomUUID().toString());
@@ -62,18 +62,18 @@ public class CarService {
 			this.em.persist(car);
 			this.em.flush();
 			this.em.refresh(car);
-		} catch(IllegalStateException exception) {
+		} catch (IllegalStateException exception) {
 			throw new CarStateNotValidException("Car state not valid");
 		}
-		
+
 		return car;
 	}
-	
+
 	public Car updateCar(Car car) {
 		Car carToUpdate = getCar(car.getId()).update(car);
 		return carToUpdate;
 	}
-	
+
 	public Car deleteCar(String id) {
 		Car carToRemove = getCar(id);
 		em.remove(carToRemove);
